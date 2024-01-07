fb6fc967c8c99e0808b438fc031454d898b3d04d
hongdayu <hong_da_yu@163.com>
2024-01-07 12:45:00 +0800

    1.update user astronvim config

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,16 @@ return {
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.debugging" },
   { import = "astrocommunity.editing-support.todo-comments-nvim" },
+  { import = "astrocommunity.editing-support.comment-box-nvim" },
+  { import = "astrocommunity.editing-support.auto-save-nvim" },
+  { import = "astrocommunity.bars-and-lines.smartcolumn-nvim" },
   { import = "astrocommunity.lsp.inc-rename-nvim" },
   { import = "astrocommunity.git.diffview-nvim" },
+  {
+    "m4xshen/smartcolumn.nvim",
+    opts = {
+      colorcolumn = 240,
+      disabled_filetypes = { "help" },
+    },
+  },
 }
